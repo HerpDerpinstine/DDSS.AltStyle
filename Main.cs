888ddf8936d19525e616272b15d8c6c3dfcb08b9
Herp Derpinstine <herpderpinstine@gmail.com>
2024-11-26 23:11:35 -0600

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -79,7 +79,7 @@ namespace DDSS_AltStyle
             if (modHelper == null)
                 return;
 
-            Type modFilterType = modHelper.MelonAssembly.Assembly.GetType("DDSS_ModHelper.Utils.RequirementFilterHandler");
+            Type modFilterType = modHelper.MelonAssembly.Assembly.GetType("DDSS_ModHelper.Utils.RequirementFilter");
             if (modFilterType == null)
                 return;
 
